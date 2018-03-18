2b16c6b8b8a713dd1df3c32ae5780039a04f69eb
Administrator <Administrator@192.168.1.104>
2018-03-19 00:53:18 +0100

    Dfs e inizio testing

---
+++ ex1/TSP/src/AI/DFS.java
@@ -0,0 +1,88 @@
+package AI;
+
+import graph.Graph;
+import graph.Node;
+import graph.Path;
+import testing.Options;
+
+public class DFS implements TSPSolver {
+
+	private Graph grafo;
+	private int counter = 0;
+	private int soluzTrovate=0;
+
+	private int[] minCammino;
+	private int minCost=Integer.MAX_VALUE;
+
+	private int[] cammino;
+	private int[] tempCosti;
+
+	public DFS(Graph g) {
+		grafo = g;
+		cammino = new int[g.getSize() + 1];
+		tempCosti = new int[g.getSize() + 1];
+	}
+
+	@Override
+	public Path process() {
+		
+		
+		for (Node n : grafo.getNodes()) {
+			if(Options.COUNTER_ON) counter++;
+			cammino[0] = n.getId();
+			tempCosti[0] = 0;
+			dfs(1, n);
+		}
+
+		return new Path(minCammino);
+	}
+
+	/**
+	 * k è il numero di vertici già inseriti nel cammino
+	 * @param k il numero di vertici già inseriti nel cammino
+	 * @param u	il vertice padre
+	 */
+	private void dfs(int k, Node u) {
+		if(Options.COUNTER_ON) counter++;
+		if (k <= grafo.getSize()) {
+			for (Node v : u.getAdjacentNodes()) {
+				
+				boolean f=false;
+				
+				for(int i=0;i<k;i++)
+					if(v.getId()==cammino[i]) {f=true; break;}
+
+				if(f) continue;
+				
+				cammino[k]=u.getId();
+				tempCosti[k]=tempCosti[k-1] + grafo.getWeight(u, v);
+				dfs(k + 1, v);
+			}
+		}
+		else {
+			for(Node v: u.getAdjacentNodes())
+			{
+				if(v.getId()==cammino[0])
+				{
+					soluzTrovate++;
+					
+					tempCosti[k]= tempCosti[k-1] + grafo.getWeight(u, v);
+					cammino[k]=cammino[0];
+					if(minCost>tempCosti[k]) {
+						for(int i=0;i<grafo.getSize()+1;i++)
+							minCammino[i]=cammino[i];
+						
+						minCost=tempCosti[k];
+						
+					}
+					
+					
+					break;
+				}
+			}
+					
+		}
+		
+	}
+
+}
--- ex1/TSP/src/testing/Testing.java
+++ ex1/TSP/src/testing/Testing.java
@@ -3,5 +3,14 @@ package testing;
 public class Testing {
 
 	
+	public static void main(String[] args) {
+		
+	}
+	
+	private static void test1()
+	{
+		
+		
+	}
 	
 }

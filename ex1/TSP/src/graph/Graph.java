75e80d652042f028b559588928b5b8b58ff44711
Administrator <Administrator@192.168.1.104>
2018-03-19 21:43:50 +0100

    Inizio importazione grafi

--- ex1/TSP/src/graph/Graph.java
+++ ex1/TSP/src/graph/Graph.java
@@ -1,15 +1,48 @@
 package graph;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Graph {
 
 	private int size;
 	private ArrayList<Node> vertex;
+	private Node[] nodes;
 	private int[][] weights;
 	
 	
-	
+	public Graph(String filename) throws IOException
+	{
+		try(BufferedReader b=new BufferedReader(new FileReader(filename));){
+			size=Integer.parseInt(b.readLine());
+			weights=new int[size][size];
+			nodes=new Node[size];
+			
+			for(int i=0;i<size;i++)
+			{
+				nodes[i]=null;
+				for(int j=0;j<size;j++)
+					weights[i][j]=0;
+			}
+			
+			String l=b.readLine();
+			while(l!=null)
+			{
+				String[] ls=l.split(",");
+				
+				if()
+				
+			}
+			
+		}
+		
+		
+		
+		
+	}
 	
 	public int getSize() {return size;}
 	public int getWeight(Node from, Node to) { return weights[from.getId()][to.getId()];}
